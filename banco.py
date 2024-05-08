98de335a1d561d9b94cf59d6b2e888e95629e879
Bianca Teixeira <biancabt1102@gmail.com>
2024-05-08 11:25:53 -0300

    feat: Function 'Depositar' implemented success

--- banco.py
+++ banco.py
@@ -13,11 +13,25 @@ LIMITE_SAQUE = 3
 extrato = ""
 numero_saques = 0
 
+def valor_invalido(valor):
+    if valor < 0:
+        print("Digite um valor válido!")
+        return True
+
+
 while True:
     opcao = input(menu)
 
     if opcao == "d":
         print("Depositar...")
+
+        valor = float(input("Digite o valor que deseja depositar: "))
+        while valor_invalido(valor):
+            valor = float(input("Digite o valor que deseja depositar: "))
+            if valor > 0:
+                saldo += valor
+                print("Valor adicionado ao saldo!")
+
     elif opcao == "s":
         print("Sacar...")
     elif opcao == "e":
