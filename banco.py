a33a2ee1131297ea6c77357d90cc76b22633fdbb
Bianca Teixeira <biancabt1102@gmail.com>
2024-05-09 11:04:27 -0300

    feat: Function 'Saque' implemented success

--- banco.py
+++ banco.py
@@ -1,10 +1,11 @@
 menu = """
-    ------------- MENU -------------
-    [d] Depositar
-    [s] Sacar
-    [e] Visualizar extrato
-    [q] Sair 
-    --------------------------------
+------------- MENU -------------
+[d] Depositar
+[s] Sacar
+[e] Visualizar extrato
+[q] Sair 
+--------------------------------
+Digite a opção:
 """
 
 saldo = 0
@@ -30,10 +31,30 @@ while True:
             valor = float(input("Digite o valor que deseja depositar: "))
             if valor > 0:
                 saldo += valor
+                extrato = f"Depósito: R$ {valor:.2f}\n"
                 print("Valor adicionado ao saldo!")
 
     elif opcao == "s":
         print("Sacar...")
+        saque = float(input("Digite o valor que deseja sacar: "))
+        excedeu_saldo = saque > saldo
+        excedeu_limite = saque > limite
+        excedeu_saques = numero_saques >= LIMITE_SAQUE
+
+        if saque > saldo:
+            print("Operação inválida! Saldo insuficiente!")
+        elif saque > limite:
+            print("Operação inválida! Valor ultrapassou o limite!")
+        elif numero_saques >= LIMITE_SAQUE:
+            print("Operação inválida! Números de saques excedido!")
+        elif saque > 0:
+            saldo -= saque
+            extrato = f"Saque: R$ {saque:.2f}\n"
+            numero_saques += 1
+            print("Saque realizado com sucesso!")
+        else:
+            print("Operação falhou! O valor informado é inválido")
+
     elif opcao == "e":
         print("Visualizar Extrato...")
     elif opcao == "q":
